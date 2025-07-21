b647b9efa5e0164b0d4bdba75a1a801cac833528
experthubowner <donjuan02876@gmail.com>
2025-07-21 22:48:46 +0200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,14 +1,17 @@
 local PlaceId = game.PlaceId
+print("🧠 PlaceId detectado:", PlaceId)
 
 local GameScripts = {
     [111958650] = "ArsenalSilent.lua"
 }
 
-local URLBase = "https://raw.githubusercontent.com/experthubowner/Experthub/main/guiones/"
+local URLBase = "https://raw.githubusercontent.com/experthubowner/Experthub/main/scripts/"
 
 if GameScripts[PlaceId] then
+    print("✅ Juego compatible, cargando script:", GameScripts[PlaceId])
     loadstring(game:HttpGet(URLBase .. GameScripts[PlaceId]))()
 else
+    print("❌ Juego no soportado (PlaceId no listado)")
     game.StarterGui:SetCore("SendNotification", {
         Title = "Expert Hub",
         Text = "Juego no soportado aún 😢",
